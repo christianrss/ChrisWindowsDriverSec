c6b01945dc9740b7fbbefe5ecaf50455271d9e01
Christian Rafael <christianparadix@gmail.com>
2025-10-25 12:23:15 -0300

    Implenting Driver Sign

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "system" fn driver_entry() -> u32 {
+    0 /* NT_STATUS SUCCESS */
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
